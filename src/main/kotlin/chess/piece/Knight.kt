10278f712b4d59f0847b58a4685460d01ccb64ca
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2023-10-01 19:20:15 +0200

    feat: main move knight

--- src/main/kotlin/chess/piece/Knight.kt
+++ src/main/kotlin/chess/piece/Knight.kt
@@ -2,12 +2,46 @@ package chess.piece
 
 import chess.Color
 import chess.square.Square
+import chess.square.direction.Horizontal
+import chess.square.direction.Vertical
 
 class Knight(
     private val color: Color,
     private val square: Square
 ): Piece(color, square) {
     override fun mainMove(): List<Square> {
-        TODO("Not yet implemented")
+        val movesList: MutableList<Square> = mutableListOf()
+
+        val maxUpMove = square.maxUpMovement()
+        val maxRightMove = square.maxRightMovement()
+        val maxDownMove = square.maxDownMovement()
+        val maxLeftMove = square.maxLeftMovement()
+
+        if (maxUpMove >= 2 && maxRightMove >= 1) {
+            movesList.add(square.move(1, 2))
+        }
+        if (maxRightMove >= 2 && maxUpMove >= 1) {
+            movesList.add(square.move(2, 1))
+        }
+        if (maxRightMove >= 2 && maxDownMove >= 1) {
+            movesList.add(square.move(2, -1))
+        }
+        if (maxDownMove >= 2 && maxRightMove >= 1) {
+            movesList.add(square.move(1, -2))
+        }
+        if (maxUpMove >= 2 && maxLeftMove >= 1) {
+            movesList.add(square.move(-1, 2))
+        }
+        if (maxLeftMove >= 2 && maxUpMove >= 1) {
+            movesList.add(square.move(-2, 1))
+        }
+        if (maxLeftMove >= 2 && maxDownMove >= 1) {
+            movesList.add(square.move(-2, -1))
+        }
+        if (maxLeftMove >= 2 && maxRightMove >= 1) {
+            movesList.add(square.move(-1, -2))
+        }
+
+        return movesList.toList()
     }
 }
